372f8f3f27b552bb1f18bf9beda43afd62014609
OmarMohamed156 <61380465+OmarMohamed156@users.noreply.github.com>
2021-05-02 17:30:05 +0200

    fixed a problem the fft

--- Task3/FFT in C++/FFT.cpp
+++ Task3/FFT in C++/FFT.cpp
@@ -10,7 +10,7 @@ typedef std::valarray<Complex> ComplexArray;
 
 void FFT(ComplexArray& data){
     const size_t  size = data.size();
-    if (size == 1 ) return;
+    if (size <= 1 ) return;
     //devide the array into even and odd based on indecies
     ComplexArray Even = data[std::slice(0,size/2,2)];
     
@@ -22,7 +22,7 @@ void FFT(ComplexArray& data){
     //calculate the complex values 
     // this part is taken from a website and i didnt get it very much
 
-    for (size_t i = 0 ; i < size ; i++)
+    for (size_t i = 0 ; i < size/2 ; i++)
     {
         Complex T = std::polar(1.0,-2*PI*i/size)*Odd[i];
         data[i] = Even[i]+T;
