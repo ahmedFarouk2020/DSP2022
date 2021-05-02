ff575773c156da7f606475d097aa71c1e6551656
ahmedFarouk2020 <ahmedfarook0111@gmail.com>
2021-05-02 18:42:11 +0200

    fix

--- Task3/FFT in C++/FFT.cpp
+++ Task3/FFT in C++/FFT.cpp
@@ -4,17 +4,10 @@
 
 const double PI = 3.14159265359;
 
-
 typedef std::complex<double> Complex;
 typedef std::valarray<Complex> ComplexArray;
-/* NOTE: THIS FUNCTION IS NOT WORKING PROPERLY!
- * NOTE: THIS FUNCTION IS NOT WORKING PROPERLY! 
- * NOTE: THIS FUNCTION IS NOT WORKING PROPERLY! 
- * NOTE: THIS FUNCTION IS NOT WORKING PROPERLY! 
- * NOTE: THIS FUNCTION IS NOT WORKING PROPERLY! 
- * NOTE: THIS FUNCTION IS NOT WORKING PROPERLY! 
- * NOTE: SEE THE VIDEO AND MIMIC HIS LOGIC
- */
+
+
 void FFT(ComplexArray& data){
     const size_t  size = data.size();
     if (size <= 1 ) return;
@@ -31,15 +24,11 @@ void FFT(ComplexArray& data){
 
     for (size_t i = 0 ; i < size/2 ; i++)
     {
-		// polar is a complex representation (mag.=1,phase=-2*PI*i/size)
-		// this for loop collect complex data(after FFT application) 
-		// "std::polar(1.0,-2*PI*i/size)" is a constatnt factor that should be multiplied by odd "see the video that you told me to see :)"
         Complex T = std::polar(1.0,-2*PI*i/size)*Odd[i];
         data[i] = Even[i]+T;
-        // the sin wave is periodic so F(k) equals F((N/2)-1 +k) and that what he did in the last line of code
-		// and the sign of T is changed each time so he adds '-' before T
-		data[i+(size/2)] = Even[i]-T;
+        data[i+(size/2)] = Even[i]-T;
 
     }
-   
+    
+
 }
