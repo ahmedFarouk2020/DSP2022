39fb94b8a01fa16c5406d0555a43bc05da60e684
ahmedFarouk2020 <ahmedfarook0111@gmail.com>
2021-05-07 17:46:16 +0200

    part2 is finished:)

---
+++ Task3/FFT in C++/DFT.cpp
@@ -0,0 +1,87 @@
+/* includes */
+#include <iostream>
+#include <vector>
+#include <complex.h>
+#include <cmath>
+#include <iterator>
+
+using namespace std;
+
+
+#define PI 3.1415926536
+typedef complex<double> Complex;
+    
+extern "C"
+{
+   void dft2(double* real_input, int N, double* real_out,double* img_out);
+}
+
+
+void dft2(double* real_input, int N, double* real_out,double* img_out)
+{
+    // container to store complex result
+    // vector<double> real_output(0);
+    // vector<double> complex_output(0);
+
+    // real, imaginary part of k-th complex element
+    double real = 0;
+    double imgn = 0;
+
+    // temperary variable to combine real, imaginary pairs inside
+    //Complex temp = 0;
+
+    // number of elements in array
+    // int N = real_input.size();
+
+    double phase, sine, cosine;
+
+    for(int k = 0; k < N; k++){
+        real_out[k] = 0;
+        img_out[k] = 0;
+        // produce one element "S(k)" of complex output
+        for(int n = 0; n < N; n++)
+        {
+            phase = (-2 * PI * k * n) / N;
+            cosine = cos(phase);
+            sine = sin(phase);
+            
+            real_out[k] += real_input[n] * cosine ;
+            img_out[k] += real_input[n] * sine ;
+
+        }
+        // real_out[k] = real;
+        // img_out[k] = imgn;
+
+        // temp.real(real);
+        // temp.imag(imgn);
+
+    // push the complex element in the output container
+        //complex_output.push_back(temp);
+    }
+    // real_out = real_output;
+    // img_out = complex_output;
+    //return complex_output;   // O(n2)
+}
+
+
+
+
+int main()
+{
+    double real_out[3], complex_out[3], input[3];
+    input[0] = {112.0};
+   int N = 3;
+    dft2(input, N, real_out, complex_out);
+    cout<< '[';
+    for(int i=0; i<N; i++)
+        cout<< '(' <<real_out[i]<< ' '<<complex_out[i] << ')' << ',';
+    cout<< ']';
+    // vector<double>::iterator i = complex_out.begin();
+    // vector<double>::iterator a = real_out.begin();
+    /* print the output */
+    // cout<<'[';
+    // for (; i != complex_out.end(); ++i)
+    //     cout << *a++ << ' '<< *i << ", ";
+    // cout<<']';
+    return 0;
+}
---
+++ Task3/FFT in C++/effeciency_test.py
@@ -0,0 +1,94 @@
+import ctypes, time
+from matplotlib import pyplot as plt
+from numpy.ctypeslib import *
+from ctypes import *
+import numpy as np
+
+class efficiencyTest():
+    def __init__(self, real_input,real_output,img_output):
+        # configure DFT library
+        DFT = ctypes.cdll.LoadLibrary(r'C:\Users\Farouk\Desktop\chocolatey\DFT_pointer.so')
+        DFT.dft2.restype = None
+        DFT.dft2.argtypes = [ndpointer(ctypes.c_double, flags="C_CONTIGUOUS"),
+                            ctypes.c_double,
+                            ndpointer(ctypes.c_double, flags="C_CONTIGUOUS"),
+                            ndpointer(ctypes.c_double, flags="C_CONTIGUOUS")]
+        self.dft = DFT.dft2
+        # configure FFT library
+        FFT = ctypes.cdll.LoadLibrary(r'C:\Users\Farouk\Desktop\chocolatey\FFTS.so')
+        FFT.fft.restype = None
+        FFT.fft.argtypes = [   ctypes.c_double,
+                                ndpointer(ctypes.c_double, flags="C_CONTIGUOUS"),
+                                ndpointer(ctypes.c_double, flags="C_CONTIGUOUS"),
+                                ndpointer(ctypes.c_double, flags="C_CONTIGUOUS")]
+        
+        self.fft = FFT.fft
+
+        #set parameters
+        self.input = real_input
+        self.real_output = real_output
+        self.img_output = img_output
+        self.DFTout = []
+        self.FFTout = []
+        self.error = []
+
+    def FFT1(self):
+        self.fft(self.input.size,self.input,self.real_output,self.img_output)
+        self.FFTout = self.real_output+ 1j * self.img_output
+        return self.FFTout
+
+    def DFT1(self):
+        self.dft(self.input,self.input.size,self.real_output,self.img_output)
+        self.DFTout = self.real_output+ 1j * self.img_output
+        return self.DFTout
+
+    def startTimer(self):
+        self.start = time.time() * 1000
+
+    def getElapsedTime(self):
+        self.elapsedTime = time.time() * 1000 - self.start
+        return self.elapsedTime
+
+    def getErrorArray(self,FFTout,DFTout):
+        
+        z = list(zip(FFTout,DFTout))
+        self.error = [x-y for x,y in z]
+        return self.error
+
+    def drawErrorPlot(self):
+        # the x axis (0 -> max index in error array)
+        x = range(0,len(self.error),1)
+        # the y axis (values of elements array)
+        y = [x.real for x in self.error]
+        plt.plot(x,y)
+        plt.show()
+
+input_ = np.array([0.0,1.0,2.0,3.0])
+real_output = np.zeros(input_.size)
+img_output = np.zeros(input_.size)
+
+a =efficiencyTest(input_,real_output,img_output)
+# a.fft(input_.size,input_,real_output,img_output)
+
+a.startTimer()
+fftout = a.FFT1()
+print(fftout)
+fftTime = a.getElapsedTime()
+
+a.startTimer()
+dftout = a.DFT1()
+print(dftout)
+dftTime = a.getElapsedTime()
+print(f" FFT time:{fftTime}ms \n DFT time:{dftTime}ms")
+
+error = a.getErrorArray(dftout,fftout)
+print(error)
+
+a.drawErrorPlot()
+
+# FFT.fft(input_.size,input_,real_output,img_output)
+# Data=real_output+ 1j * img_output
+
+# print(np.round(Data, decimals=1))
+
+
